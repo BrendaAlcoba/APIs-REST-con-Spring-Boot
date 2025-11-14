6ac010bfff9d5a001119e68b58c5a133088e87e8
BrendaAlcoba <106177189+BrendaAlcoba@users.noreply.github.com>
2025-11-14 11:51:21 -0300

    Corrección validaciones de Producto y actualización de pom para compatibilidad

--- src/main/java/com/utn/productos_api/model/Producto.java
+++ src/main/java/com/utn/productos_api/model/Producto.java
@@ -2,7 +2,8 @@ package com.utn.productos_api.model;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -17,20 +18,15 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "El nombre no puede estar vacío")
-    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres") // APLICAR ESTE CAMBIO
     private String nombre;
 
-    @Size(max = 500, message = "La descripción no debe superar los 500 caracteres") // APLICAR ESTE CAMBIO
     private String descripcion;
 
-    @Positive(message = "El precio debe ser mayor a 0")
     private double precio;
 
-    @Min(value = 0, message = "El stock no puede ser negativo")
     private int stock;
 
-    @NotNull(message = "La categoría es obligatoria")
-    @Enumerated(EnumType.STRING)   // @Enumerated
+
+    @Enumerated(EnumType.STRING)
     private Categoria categoria;
 }
